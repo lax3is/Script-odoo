f7c8fccbf3569bc179655aff9f00d10ff49eb27c
lax3is <zelda083@hotmail.fr>
2025-04-24 14:35:03 +0200

    Update Bouton Traiter l'Appel Odoo.js

--- Bouton Traiter l'Appel Odoo.js
+++ Bouton Traiter l'Appel Odoo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bouton Traiter l'Appel Odoo
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Ajoute un bouton "Traiter l'appel" avec texte clignotant
 // @author       Alexis.sair
 // @match        https://winprovence.odoo.com/*
